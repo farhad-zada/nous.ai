aff7aad6e0f5858a906a1f5f9cdeae951a7ca1d2
Emin Suleymanov <suleymanov.emin2001@gmail.com>
2023-10-20 00:12:45 +0400

    test

--- client/app/register/page.js
+++ client/app/register/page.js
@@ -75,7 +75,7 @@ const Page = () => {
               className="bg-blue-500 hover:bg-blue-700 text-white transition-all font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
             >
-              Sign up
+              Sign up?
             </button>
             <Link
               className="inline-block align-baseline font-bold text-sm transition-all text-blue-500 hover:text-blue-800"
